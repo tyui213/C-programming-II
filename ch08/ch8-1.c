b7cfd04fa51cbb9af62c9238c5a64671b2a325ba
tyui213 <2743588jm@naver.com>
2025-09-26 19:11:28 +0900

    0926

---
+++ ch08/ch8-1.c
@@ -0,0 +1,22 @@
+/*
+학번 202511241
+이름 최정민
+프로그램명 포인터의 바이트 크기 구하기
+날짜 0926
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	int* pi;
+	double* pd;
+	char* pc;
+
+
+	printf("sizeof(pi) = %zd\n", sizeof(pi));
+	printf("sizeof(pd) = %zd\n", sizeof(pd));
+	printf("sizeof(pc) = %zd\n", sizeof(pc));
+
+
+}
