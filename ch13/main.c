3db0678d4b2d5f24504c858c42b5d65150c1c4f3
tyui213 <2743588jm@naver.com>
2025-11-11 14:40:59 +0900

    gfchvjb

---
+++ ch13/main.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+#include "point.h"
+
+int main(int argc,char * argv[])
+{
+	for (int i = 0; i < argc; i++)
+	{
+		printf("%s", i, argv[i]);
+	}
+	if (argc >= 3) {
+		int a[1] = atoi(argv[1]);
+		int a[2] = atoi(argv[2]);
+		int a[3] = atoi(argv[3]);
+	
+	printf("\n");
+	
+	for (int i = 0; i < a3; i++)
+	{
+		
+	}
+
+	if (strcmp(argv[1], "1") == 0)
+	{
+
+	}
+
+	printf("Hello, I love you!\n");
+	return 0;
+}
---
+++ ch13/point.h
@@ -0,0 +1,7 @@
+#ifndef _POINT_H_123123
+#define _POINT_H_123123
+typedef struct {
+	int x;
+	int y;
+} Point;
+#endif  // _POINT_H_123123
